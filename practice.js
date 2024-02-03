e2f8e92813a8669daffbc8871182998b65fa6917
Joy <github.joysaha@gmail.com>
2024-02-03 17:15:41 +0600

    practice task 3 complete

--- practice.js
+++ practice.js
@@ -14,14 +14,36 @@
 ////////////////////// Task 2 /////////////////////
 
 
-function takeNumber(numbers){
-    if(numbers % 2 === 1){
-        return numbers * 2;
+// function takeNumber(numbers){
+//     if(numbers % 2 === 1){
+//         return numbers * 2;
+//     }
+//     else{
+//         return numbers / 2;
+//     }
+// }
+// let numbers = 4;
+// let result = takeNumber(numbers);
+// console.log("result :", result);
+
+////////////////// Task 3 //////////////////////////
+
+function make_avg(myArr, size){
+    if(size === 0 || myArr.length === 0){
+        return 0;
     }
-    else{
-        return numbers / 2;
+
+    let sum = 0;
+    for(let i = 0; i < size; i++){
+        sum = sum + myArr[i];
     }
+    
+    let average = sum / size;
+    return average;
 }
-let numbers = 4;
-let result = takeNumber(numbers);
-console.log("result :", result);
+
+let arr = [34, 44, 91, 20, 47, 65];
+let arrSize = arr.length;
+
+let result = make_avg(arr, arrSize);
+console.log("result", result);
